57dc9a88eb8f1ad5caefe0ceb90aed7c8617a5ec
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-01-13 05:44:05 -0800

    AirBnB clone project

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Defines the HBnB console."""
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
